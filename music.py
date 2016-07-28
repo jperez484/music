e9ba3b3d24c5c40f9531b52cd723f4e34984ff40
Jesus Infante <jperez484@gmail.com>
2016-07-28 01:47:09 +0000

    update music

--- music.py
+++ music.py
@@ -1,46 +1,53 @@
 class Musician(object):
-    def __init__(self, sounds):
+    
+    def __init__(self, sounds, name):
         self.sounds = sounds
+        self.name = name
 
     def solo(self, length):
+        music =[]
         for i in range(length):
-            print(self.sounds[i % len(self.sounds)], end=" ")
-        print()
+            music.append(self.sounds[i % len(self.sounds)])
+            #print(self.sounds[i % len(self.sounds)], end=" ")
+        #print()
+        return music
         
 
 class Bassist(Musician): # The Musician class is the parent of the Bassist class
     def __init__(self):
         # Call the __init__ method of the parent class
-        super().__init__(["Twang", "Thrumb", "Bling"])
+        super(Bassist, self).__init__(["Twang", "Thrumb", "Bling"], 'bassist')
 
 class Guitarist(Musician):
     def __init__(self):
         # Call the __init__ method of the parent class
-        super().__init__(["Boink", "Bow", "Boom"])
+        super(Guitarist, self).__init__(["Boink", "Bow", "Boom"], 'guitarist')
 
     def tune(self):
         print("Be with you in a moment")
         print("Twoning, sproing, splang")
         
+        
+class Drummer(Musician):
+    def __init__(self):
+        super(Drummer, self).__init__(["Pa", "Rum", "Pum", "Pum", "Pum", "Ba", "Bum"], 'drummer')
+        #self.sounds = ["Pa", "Rum", "Pum", "Pum", "Pum", "Ba", "Bum"]
+
+    def solo(self, length):
+        for i in range(length):
+            print(self.sounds[i % len(self.sounds)], end=" ")
+        print("And a ONE, and a TWO, and a Three, FOUR, BOOOOOOOOOOOOOM")
+        
 class Pianist(Musician):
     def __init__(self):
-        self.sounds = ["Din", "Din", "Din", "Din", "Din, ""Dun", "Dunnnnnn"]
+        super(Pianist, self).__init__(["Din", "Din", "Din", "Din", "Din, ""Dun", "Dunnnnnn"], 'pianist')
+        #self.sounds = ["Din", "Din", "Din", "Din", "Din, ""Dun", "Dunnnnnn"]
 
     def solo(self, length):
         for i in range(length):
             print(self.sounds[i % len(self.sounds)], end=" ")
         print("Let's get classical...")
         print("Dun, Dun, Din, Din")
-        
-        
-class Drummer(Musician):
-    def __init__(self):
-        self.sounds = ["Pa", "Rum", "Pum", "Pum", "Pum", "Ba", "Bum"]
-
-    def solo(self, length):
-        for i in range(length):
-            print(self.sounds[i % len(self.sounds)], end=" ")
-        print("And a ONE, and a TWO, and a Three, FOUR, BOOOOOOOOOOOOOM")
   
 Band_hire = {
         Guitarist: "Do you want a Guitarist?",
@@ -52,7 +59,7 @@ class Band:
     def __init__(self):
         self.members = []
 
-    def interactive_hire(self):
+    def hire(self, player):
     
         for player in Band_hire: 
             answer =input(Band_hire[player])
@@ -65,6 +72,6 @@ class Band:
                 self.members.append(band_member)
 
 New_Band = Band()
-New_Band.interactive_hire()
+New_Band.hire()
 
   
